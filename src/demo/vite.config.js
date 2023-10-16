b8f3080ac0cc901e0f9786d3b42d69cca61c7fa8
FilipekBenes <83021112+FilipekBenes@users.noreply.github.com>
2023-10-16 09:42:09 +0200

    new app

---
+++ src/demo/vite.config.js
@@ -0,0 +1,38 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  build: {
+    outDir: "../../dist/demo",
+    assetsDir: "./assets",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/scss/_variables.scss";
+        @import "./src/assets/scss/_mixins.scss";
+        @import "./src/assets/scss/_placeholders.scss";
+        `
+      },
+    },
+  },
+})
