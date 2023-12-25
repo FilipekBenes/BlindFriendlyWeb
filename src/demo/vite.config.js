abc692d99c0c60ff99854d1bcf911de11842ab05
FilipekBenes <83021112+FilipekBenes@users.noreply.github.com>
2023-12-25 22:31:21 +0100

    Upgrade to new version

--- src/demo/vite.config.js
+++ src/demo/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/blind-friendly-library/",
+  base: "/BlindFriendlyWeb/",
   plugins: [
     vue(),
   ],
