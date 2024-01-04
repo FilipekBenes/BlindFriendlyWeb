f1d5c07f6a3fdf31db63ceda1f92d7b9201b763a
FilipekBenes <83021112+FilipekBenes@users.noreply.github.com>
2024-01-04 14:29:30 +0100

    UPDATE: new scripts

--- src/demo/vite.config.js
+++ src/demo/vite.config.js
@@ -5,12 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BlindFriendlyWeb/",
+  base: "./",
   plugins: [
     vue(),
   ],
   build: {
-    outDir: "../../dist/demo",
+    outDir: "../../dist",
     assetsDir: "./assets",
     rollupOptions: {
       output: {
